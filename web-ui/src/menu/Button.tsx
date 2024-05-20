162a5f8e23e8123e991beb004854ea92bed9e33e
geoffolson <geoffrey.olson.jr@gmail.com>
2024-05-20 15:16:06 -0700

    fix styling on disabled button

--- web-ui/src/menu/Button.tsx
+++ web-ui/src/menu/Button.tsx
@@ -1,16 +1,20 @@
+import classNames from "classnames";
+
 export const Button = (
   props: React.DetailedHTMLProps<
     React.ButtonHTMLAttributes<HTMLButtonElement>,
     HTMLButtonElement
-  > & { variant?: "primary" | "secondary" }
+  > & { variant?: "primary" | "secondary"; isLoading?: boolean }
 ) => {
-  if (props.variant === "secondary") {
-  }
   return (
     <button
-      className={`border-gray-800 bg-gray-800 border-2 ${
-        props.disabled ? "bg-gray-600 text-gray-800" : ""
-      } ${props.variant === "secondary" ? "bg-transparent text-gray-800" : ""} p-3 w-full`}
+      className={classNames(
+        "border-gray-800 bg-gray-800 border-2 p-3 w-full",
+        {
+          "bg-gray-600 text-gray-500": props.disabled || props.isLoading,
+        },
+        { "bg-transparent text-gray-800": props.variant === "secondary" }
+      )}
       {...props}
     />
   );
