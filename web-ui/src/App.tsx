0c86ffab81a543f511c224db14089ef9dc78a3b8
geoffolson <geoffrey.olson.jr@gmail.com>
2024-05-21 19:57:16 -0700

    fix issue with losing keyboard controls when game loses focus

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -4,34 +4,36 @@ import { Stats } from "./Stats";
 import { Map } from "./Map";
 import { Modal } from "./menu/Modal";
 import { World } from "./World";
+import { useEffect } from "react";
 
 function App() {
   const dispatch = useDispatch();
+  const onKeyDown = (e: KeyboardEvent) => {
+    switch (e.code) {
+      case "ArrowUp":
+        dispatch(move(PlayerInput.Up));
+        break;
+      case "ArrowDown":
+        dispatch(move(PlayerInput.Down));
+        break;
+      case "ArrowLeft":
+        dispatch(move(PlayerInput.Left));
+        break;
+      case "ArrowRight":
+        dispatch(move(PlayerInput.Right));
+        break;
+      case "Escape":
+      case "Space":
+        dispatch(move(PlayerInput.Pause));
+        break;
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, [dispatch]);
   return (
-    <div
-      className="h-screen bg-sky-900"
-      tabIndex={0}
-      onKeyDown={(e) => {
-        switch (e.code) {
-          case "ArrowUp":
-            dispatch(move(PlayerInput.Up));
-            break;
-          case "ArrowDown":
-            dispatch(move(PlayerInput.Down));
-            break;
-          case "ArrowLeft":
-            dispatch(move(PlayerInput.Left));
-            break;
-          case "ArrowRight":
-            dispatch(move(PlayerInput.Right));
-            break;
-          case "Escape":
-          case "Space":
-            dispatch(move(PlayerInput.Pause));
-            break;
-        }
-      }}
-    >
+    <div className="h-screen bg-sky-900" tabIndex={0}>
       <World />
       <div className="flex flex-col w-full bg-gray-950">
         <div className="flex w-full content-between justify-between bg-blue-500"></div>
