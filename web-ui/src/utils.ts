a7fabded98b7aaf59625a9b308a972573bf6ab07
geoffolson <geoffrey.olson.jr@gmail.com>
2024-05-22 02:52:23 -0700

    remove unused function

--- web-ui/src/utils.ts
+++ web-ui/src/utils.ts
@@ -1,27 +1,7 @@
 import { vector } from "./redux/gameSlice";
 import { saveGameKey } from "./redux/middleware";
-import { useEffect, useState } from "react";
 
 // 2D vector equality
 export const eq = (A: vector, B: vector) => A.x === B.x && A.y === B.y;
 
 export const hasLocalSavedGame = () => !!window.localStorage.getItem(saveGameKey);
-
-export const useOnWindowResize = (callback: Function, deps: any[]) => {
-  const [isResized, setIsResized] = useState(false);
-
-  useEffect(() => {
-    const onResize = () => {
-      setIsResized((s) => !s);
-      callback();
-    };
-    callback();
-    window.addEventListener("resize", onResize);
-
-    return () => {
-      window.removeEventListener("resize", onResize);
-    };
-  }, [callback, ...deps]);
-
-  return isResized;
-};
