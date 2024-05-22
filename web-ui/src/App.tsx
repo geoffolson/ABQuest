87781dd7aa978c8fa55d96e9cd1fdf92bc6e89e7
geoffolson <geoffrey.olson.jr@gmail.com>
2024-05-21 20:00:24 -0700

    remove unused css classes

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -38,7 +38,7 @@ function App() {
       <div className="flex flex-col w-full bg-gray-950">
         <div className="flex w-full content-between justify-between bg-blue-500"></div>
         <Modal />
-        <div className="fixed z-10 inset-0 bg-gray-900 bg-transparent overflow-y-auto h-full w-full px-4">
+        <div className="fixed z-10 inset-0 bg-transparent px-4">
           <Stats />
           <Map />
         </div>
