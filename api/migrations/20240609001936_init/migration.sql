bef2d530fd303e952fe4dfa98527dadbb0838352
geoffolson <geoffrey.olson.jr@gmail.com>
2024-06-08 17:25:50 -0700

    setup postgres in local docker environment

--- api/migrations/20240609001936_init/migration.sql
+++ api/migrations/20240609001936_init/migration.sql
@@ -1,23 +1,29 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "savedGameId" INTEGER,
-    CONSTRAINT "User_savedGameId_fkey" FOREIGN KEY ("savedGameId") REFERENCES "SavedGame" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "SavedGame" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "seed" INTEGER NOT NULL,
     "positionX" INTEGER NOT NULL,
     "positionY" INTEGER NOT NULL,
     "endPositionX" INTEGER NOT NULL,
     "endPositionY" INTEGER NOT NULL,
     "health" INTEGER NOT NULL,
-    "moves" INTEGER NOT NULL
+    "moves" INTEGER NOT NULL,
+
+    CONSTRAINT "SavedGame_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_savedGameId_fkey" FOREIGN KEY ("savedGameId") REFERENCES "SavedGame"("id") ON DELETE SET NULL ON UPDATE CASCADE;
